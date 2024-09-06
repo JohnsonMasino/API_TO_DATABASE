cf3f73b69cb0fea1e9d798e6eef2cae486286236
Johnson Masino <johnsonmasino@gmail.com>
2024-09-06 10:34:24 +0100

    Adding the Login Feature

---
+++ Api_To_Database/app/Http/Controllers/Action/LoginAction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Action;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class LoginAction
+{
+    public function handle(LoginRequest $request)
+    {
+        $user = User::query()->where('email', '=', $request->validated('email'))->first();
+        if (Hash::check($request->validated('password'), $user->password)) {
+            $user->login();
+            return successfulResponse([
+                'data' => [
+                    'user' => new UserResource($user)
+                ]
+            ], 'Login successful');
+        }
+
+        return errorResponse('Wrong Credentials !, Try again.', ResponseAlias::HTTP_BAD_REQUEST);
+
+    }
+}
